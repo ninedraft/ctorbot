910de3a8a2ada8df914f42c1a31113bf0a8bd98a
ninedraft <clamores.pro@gmail.com>
2018-11-02 21:15:22 +0300

    add proxy support to bot

--- pkg/telegram/bot.go
+++ pkg/telegram/bot.go
@@ -4,16 +4,36 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/hekmon/transmissionrpc"
+	"golang.org/x/net/proxy"
 	"gopkg.in/telegram-bot-api.v4"
 )
 
 type BotConfig struct {
 	TelegramToken string
+	ProxyConfig
+}
+
+type ProxyConfig struct {
+	ProxyAddress  string
+	ProxyUsername string
+	ProxyPassword string
+}
+
+func (pconfig ProxyConfig) IsEmpty() bool {
+	return pconfig == (ProxyConfig{})
+}
+
+func (pconfig ProxyConfig) ProxyAuth() *proxy.Auth {
+	return &proxy.Auth{
+		User:     pconfig.ProxyUsername,
+		Password: pconfig.ProxyPassword,
+	}
 }
 
 type Bot struct {
@@ -35,6 +55,18 @@ func New(config BotConfig) (Bot, error) {
 	var tgClient, newBotErr = tgbotapi.NewBotAPI(config.TelegramToken)
 	if newBotErr != nil {
 		return Bot{}, newBotErr
+	}
+	if !config.IsEmpty() {
+		var dialer, errSOCKS5 = proxy.SOCKS5("tcp",
+			config.ProxyAddress,
+			config.ProxyAuth(),
+			proxy.Direct)
+		if errSOCKS5 != nil {
+			return Bot{}, errSOCKS5
+		}
+		tgClient.Client.Transport = &http.Transport{
+			Dial: dialer.Dial,
+		}
 	}
 	return Bot{
 		transmission: torrentClient,
