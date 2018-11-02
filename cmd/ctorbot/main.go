b86dee77ced349fced7c4ac95226b77feb14d0de
ninedraft <clamores.pro@gmail.com>
2018-11-02 20:58:41 +0300

    add main func execution

--- cmd/ctorbot/main.go
+++ cmd/ctorbot/main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/ninedraft/ctorbot/pkg/telegram"
+)
+
 func main() {
+	var bot, errNewBot = telegram.New(telegram.BotConfig{})
+	if errNewBot != nil {
+		log.Fatal(errNewBot)
+	}
+	if err := bot.Run(context.Background()); err != nil {
+		log.Fatal(err)
+	}
 }
